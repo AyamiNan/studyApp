18bccd062260c18e4530c2446323bfbd62e77149
AyamiNan <orangepottea@gmail.com>
2022-08-15 23:21:46 +0900

    third commit

--- database/migrations/2022_08_14_084713_create_todos_table.php
+++ database/migrations/2022_08_14_084713_create_todos_table.php
@@ -1,10 +1,11 @@
 <?php
-
+//課題テキストと順番が違うだけ
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+//return new class extends Migration
+class CreateTodosTable extends Migration;
 {
     /**
      * Run the migrations.
@@ -15,7 +16,10 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->text('title'); /* 追加 */
+            $table->('content'); /* 追加 */
             $table->timestamps();
+            $table->softDeletes(); /* 追加 */
         });
     }
 

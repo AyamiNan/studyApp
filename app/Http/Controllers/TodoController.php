b8ce8d0f892c9d672dcd308ea9f9572bf744ae5b
AyamiNan <orangepottea@gmail.com>
2022-08-16 22:59:18 +0900

    0816 commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1); //追記
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+   /**
+    * constructor function
+    */
+    public function __construct() //追記
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Hello Worldを出力
+       // return "Hello world!!";
+       //blade.php作ったのでControllerの修正
+       return view('layouts.app');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(int $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        //
+    }
+}
--- database/migrations/2022_08_14_084713_create_todos_table.php
+++ database/migrations/2022_08_14_084713_create_todos_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 //return new class extends Migration
-class CreateTodosTable extends Migration;
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,7 @@ class CreateTodosTable extends Migration;
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->text('title'); /* 追加 */
-            $table->('content'); /* 追加 */
+            $table->text('content');  /* 追加 */
             $table->timestamps();
             $table->softDeletes(); /* 追加 */
         });

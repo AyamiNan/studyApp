8dc34f89a98a34f1a27d3270c70185adbba097df
AyamiNan <orangepottea@gmail.com>
2022-09-05 15:10:21 +0900

    0905commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
@@ -30,9 +30,9 @@
           </thead>
           <tbody>
             <tr>
-              <td class="border px-4 py-2"></td>
-              <td class="border px-4 py-2"></td>
-              <td class="border px-4 py-2"></td>
+              <td class="border px-4 py-2">初めてのTODOです</td>
+              <td class="border px-4 py-2">2021-02-01 00:00:00</td>
+              <td class="border px-4 py-2">2021-02-01 00:00:00</td>
               <td class="border px-4 py-2"></td>
             </tr>
           </tbody>

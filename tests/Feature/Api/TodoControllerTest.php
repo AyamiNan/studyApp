9cd7f73dd14d26ad7e242170d03232028ca05743
AyamiNan <orangepottea@gmail.com>
2022-09-26 01:05:15 +0900

    0926新規作成失敗テスト

--- tests/Feature/Api/TodoControllerTest.php
+++ tests/Feature/Api/TodoControllerTest.php
@@ -26,7 +26,28 @@ class TodoControllerTest extends TestCase
      * @test
      */
 
-    public function Todoの新規作成()
+    //public function Todoの新規作成() //TodoチェックリストアプリのTodoの新規作成についてのテスト
+    //{
+        //$params = [
+        //    'title' =>'テスト:タイトル',
+        //    'content' => 'テスト:内容'
+        //];
+       // $res = $this->postJson(route('api.todo.create'), $params);
+       // $res->assertOk();
+       // $todos = Todo::all();
+
+        //$this->assertCount(1, $todos);
+
+        //$todo= $todos->first();
+
+       // $this->assertEquals($params['title'], $todo->title);
+        //$this->assertEquals($params['content'], $todo->content);
+    //}
+
+//課題
+//Api実装
+//テスト（更新/詳細取得/削除）成功/失敗
+public function Todoの新規作成失敗() //新規処理のテスト失敗ケース
     {
         $params = [
             'title' =>'テスト:タイトル',
@@ -36,7 +57,7 @@ class TodoControllerTest extends TestCase
         $res->assertOk();
         $todos = Todo::all();
 
-        $this->assertCount(1, $todos);
+        $this->assertCount(2, $todos);
 
         $todo= $todos->first();
 
